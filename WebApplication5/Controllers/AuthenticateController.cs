386ce0f556b731c6a19ea5b46c75466c22128a67
Dasari veera <92787708+Dasariveera222@users.noreply.github.com>
2024-09-12 10:02:11 +0530

    Add files via upload

---
+++ WebApplication5/Controllers/AuthenticateController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+
+namespace WebApplication5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        ProductDbContext _context;
+        public AuthenticateController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost]
+
+        public IActionResult  Login(String uname, string pswd)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.UserName == uname);
+            var password = _context.Users.SingleOrDefault(u => u.Password == pswd);
+            if (user == null)
+            {
+                // User not found
+                return Unauthorized("Invalid username or password.");
+
+            }
+            else
+            {
+                if (password == null) 
+                {
+                    return Unauthorized("Invalid username or password.");
+                }
+                else
+                {
+                    return Ok("Login successful.");
+                }
+            }
+        }
+
+
+
+
+    }
+
+
+}
---
+++ WebApplication5/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication5.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Users> Users { get; set; }
+        
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+         : base(options)
+        {
+
+        }
+    }
+}
